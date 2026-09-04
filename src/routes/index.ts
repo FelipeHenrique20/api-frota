4c4876115a982561afe829d03a6b632e871d471c
Felipe Henrique Sousa Filgueira <fiphg49@gmail.com>
2026-09-04 11:19:00 -0300

    feat: registra rotas de clientes e alugueis no roteador principal

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
 import veiculoRoutes from "../veiculo/veiculoRoutes.js";
+import clienteRoutes from "../cliente/clienteRoutes.js";
+import aluguelRoutes from "../aluguel/aluguelRoutes.js";
 
 const routes = Router();
 
 routes.use("/veiculos", veiculoRoutes);
+routes.use("/clientes", clienteRoutes);
+routes.use("/alugueis", aluguelRoutes);
 
 export default routes;
